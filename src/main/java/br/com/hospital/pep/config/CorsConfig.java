8e4f553e84323f032cafc2dde92c4bf419e32c06
ClavaJava <victor.castelhano@hotmail.com>
2026-03-08 16:23:36 -0400

    fix: cors produção

--- src/main/java/br/com/hospital/pep/config/CorsConfig.java
+++ src/main/java/br/com/hospital/pep/config/CorsConfig.java
@@ -9,8 +9,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://18.220.72.36:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
                 .allowedHeaders("*");
     }
 }
